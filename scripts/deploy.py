c359f63b8d5da35257aee0056691ae24e526a0e8
webelf101 <m63974529@gmail.com>
2022-01-19 15:48:59 +0800

    initial commit

---
+++ scripts/deploy.py
@@ -0,0 +1,10 @@
+from brownie import CellarPoolShare, accounts
+
+def main():
+    acct = accounts.load("deployer_account")
+    name = "Volume WETH USDT 0.05 Cellar"
+    symbol = "VOLWETHUSDT05"
+    token0 = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    token1 = "0xdac17f958d2ee523a2206206994597c13d831ec7"
+    cellarTickInfo = [[0, -192520, -199360, 1]]
+    CellarPoolShare.deploy(name, symbol, token0, token1, 500, cellarTickInfo, {"from":acct})
