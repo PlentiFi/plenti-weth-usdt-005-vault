264772cc13d33a665fd67962b8e58e172dd8a1c5
webelf101 <m63974529@gmail.com>
2022-01-26 04:49:35 +0800

    update gravity bridge address

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,5 +6,8 @@ def main():
     symbol = "PLENTIWETHUSDT05"
     token0 = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
     token1 = "0xdac17f958d2ee523a2206206994597c13d831ec7"
+    gravity_bridge = "0x69592e6f9d21989a043646fE8225da2600e5A0f7"
     cellarTickInfo = [[0, -192520, -199360, 1]]
-    CellarPoolShare.deploy(name, symbol, token0, token1, 500, cellarTickInfo, {"from":acct})
+    CellarPoolShareContract = CellarPoolShare.deploy(name, symbol, token0, token1, 500, cellarTickInfo, {"from":acct})
+    CellarPoolShareContract.setAdjuster(gravity_bridge, True, {"from": acct})
+    CellarPoolShareContract.setValidator(gravity_bridge, True, {"from": acct})
